2f6908287a4fef3f2cff25f40df838b6e489005e
Gábor IMRE <imre.gabesz@gmail.com>
2016-03-16 09:23:51 +0100

    fix(ts build): missing definitions

---
+++ includes.d.ts
@@ -0,0 +1,14 @@
+declare module 'colors/safe' {
+	export function setTheme(theme:any):any;
+
+	export function black(text: string): string;
+	export function red(text: string): string;
+	export function green(text: string): string;
+	export function yellow(text: string): string;
+	export function blue(text: string): string;
+	export function magenta(text: string): string;
+	export function cyan(text: string): string;
+	export function white(text: string): string;
+	export function gray(text: string): string;
+	export function grey(text: string): string;
+}
