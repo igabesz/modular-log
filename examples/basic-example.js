af6207531f454247bc299c3c5e0c903f87adc1fa
Gábor IMRE <imre.gabesz@gmail.com>
2016-03-16 10:52:19 +0100

    fix(examples): according to refactor

--- examples/basic-example.js
+++ examples/basic-example.js
@@ -1,13 +1,16 @@
 "use strict";
-var log = require('../index');
+var log = require('../lib/index');
+// Create project level console log
 log.createConsoleLogger({
     level: 'trace',
 });
+// Cretate project level file log
 log.createFileLogger({
     filename: 'outfile.log',
     level: 'warn',
 });
-var cnt = 0;
+var cnt = 0; // Helper variable
+// First logger object
 var logger = log.createLogger('App');
 logger.fatal('Testing', { cnt: cnt++ });
 logger.error('Testing', { cnt: cnt++ });
@@ -15,7 +18,8 @@ logger.warn('Testing', { cnt: cnt++ });
 logger.info('Testing', { cnt: cnt++ });
 logger.debug('Testing', { cnt: cnt++ });
 logger.trace('Testing', { cnt: cnt++ });
+// Preparing delayed log
 setTimeout(function () { return logger.fatal('Testing', { cnt: cnt++ }); }, 1000);
+// Second logger object
 var logger2 = log.createLogger('Another');
 logger2.warn('Another guy here');
-//# sourceMappingURL=basic-example.js.map
--- examples/basic-example.ts
+++ examples/basic-example.ts
@@ -1,15 +1,19 @@
-import * as log from '../index';
+import * as log from '../lib/index';
 
+// Create project level console log
 log.createConsoleLogger({
 	level: 'trace',
 });
 
+// Cretate project level file log
 log.createFileLogger({
 	filename: 'outfile.log',
 	level: 'warn',
 });
 
-let cnt = 0;
+let cnt = 0; // Helper variable
+
+// First logger object
 let logger = log.createLogger('App');
 logger.fatal('Testing', { cnt: cnt++ });
 logger.error('Testing', { cnt: cnt++ });
@@ -18,7 +22,9 @@ logger.info('Testing', { cnt: cnt++ });
 logger.debug('Testing', { cnt: cnt++ });
 logger.trace('Testing', { cnt: cnt++ });
 
+// Preparing delayed log
 setTimeout(() => logger.fatal('Testing', { cnt: cnt++ }), 1000);
 
+// Second logger object
 let logger2 = log.createLogger('Another');
 logger2.warn('Another guy here');
