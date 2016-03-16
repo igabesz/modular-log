fa5ad4461089d831d4b7d99b663b3e268ef48ccd
Gábor IMRE <imre.gabesz@gmail.com>
2016-03-16 11:04:30 +0100

    fix(definitions): meta is not required

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,11 +1,11 @@
 import * as winston from 'winston';
 export interface Logger extends winston.LoggerInstance {
-    fatal(msg: string, meta: any, callback?: () => void): Logger;
-    error(msg: string, meta: any, callback?: () => void): Logger;
-    warn(msg: string, meta: any, callback?: () => void): Logger;
-    info(msg: string, meta: any, callback?: () => void): Logger;
-    debug(msg: string, meta: any, callback?: () => void): Logger;
-    trace(msg: string, meta: any, callback?: () => void): Logger;
+    fatal(msg: string, meta?: any, callback?: () => void): Logger;
+    error(msg: string, meta?: any, callback?: () => void): Logger;
+    warn(msg: string, meta?: any, callback?: () => void): Logger;
+    info(msg: string, meta?: any, callback?: () => void): Logger;
+    debug(msg: string, meta?: any, callback?: () => void): Logger;
+    trace(msg: string, meta?: any, callback?: () => void): Logger;
 }
 export declare const levels: {
     fatal: number;
